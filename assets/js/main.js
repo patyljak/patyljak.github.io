e0f1acb8dfd600f1a59a661b84a78c71cfb87de0
milosC <milos92@gmx.com>
2022-11-20 17:13:52 +0100

    Connection Dance Studio

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,8 @@
             loop: true,
             autoplay: true,
             nav: true,
-            dots: false,
+            autoplayTimeout:50000,
+            dots: true,
             navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
             responsive:{
                 0:{
@@ -80,7 +81,7 @@
             $('.time-countdown').each(function() {
             var $this = $(this), finalDate = $(this).data('countdown');
             $this.countdown(finalDate, function(event) {
-                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span><span class="timer-label">Dana</span></div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span><span class="timer-label">Sati</span></div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span><span class="timer-label">Minuta</span></div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span><span class="timer-label">Sekundi</span></div></div>'));
             });
          });
         }
@@ -156,6 +157,82 @@
         $(".close-btn").on("click", function() {
             $(".search-area").removeClass("search-active");
         });
+
+        // jQuery("#nanogallery3").nanogallery2({
+        
+        //     // CONTENT SOURCE
+        //     // The optional add-on nanoPhotosProvider is used for this example - this is not mandatory and can easily be replaced by a list of medias
+        //     items: [
+        //         { src: '../assets/img/test2.jpg', srct: '../assets/img/test2.jpg', title: 'Berlin 1' },
+        //         { src: '../assets/img/test2.jpg', srct: '../assets/img/test2.jpg', title: 'Berlin 2' },
+        //         { src: '../assets/img/test2.jpg', srct: '../assets/img/test2.jpg', title: 'Berlin 3' }
+        //       ],
+        //     album: 'people',
+           
+        //     // GALLERY AND THUMBNAIL LAYOUT
+        //     galleryMosaic : [                       // default layout
+        //       { w: 2, h: 2, c: 1, r: 1 },
+        //       { w: 1, h: 1, c: 3, r: 1 },
+        //       { w: 1, h: 1, c: 3, r: 2 },
+        //       { w: 1, h: 2, c: 4, r: 1 },
+        //       { w: 2, h: 1, c: 5, r: 1 },
+        //       { w: 2, h: 2, c: 5, r: 2 },
+        //       { w: 1, h: 1, c: 4, r: 3 },
+        //       { w: 2, h: 1, c: 2, r: 3 },
+        //       { w: 1, h: 2, c: 1, r: 3 },
+        //       { w: 1, h: 1, c: 2, r: 4 },
+        //       { w: 2, h: 1, c: 3, r: 4 },
+        //       { w: 1, h: 1, c: 5, r: 4 },
+        //       { w: 1, h: 1, c: 6, r: 4 }
+        //     ],
+        //     galleryMosaicXS : [                     // layout for XS width
+        //       { w: 2, h: 2, c: 1, r: 1 },
+        //       { w: 1, h: 1, c: 3, r: 1 },
+        //       { w: 1, h: 1, c: 3, r: 2 },
+        //       { w: 1, h: 2, c: 1, r: 3 },
+        //       { w: 2, h: 1, c: 2, r: 3 },
+        //       { w: 1, h: 1, c: 2, r: 4 },
+        //       { w: 1, h: 1, c: 3, r: 4 }
+        //     ],
+        //     galleryMosaicSM : [                     // layout for SM width
+        //       { w: 2, h: 2, c: 1, r: 1 },
+        //       { w: 1, h: 1, c: 3, r: 1 },
+        //       { w: 1, h: 1, c: 3, r: 2 },
+        //       { w: 1, h: 2, c: 1, r: 3 },
+        //       { w: 2, h: 1, c: 2, r: 3 },
+        //       { w: 1, h: 1, c: 2, r: 4 },
+        //       { w: 1, h: 1, c: 3, r: 4 }
+        //     ],
+        //     galleryMaxRows: 1,
+        //     galleryDisplayMode: 'rows',
+        //     gallerySorting: 'random',
+        //     thumbnailDisplayOrder: 'random',
+        //     thumbnailHeight: '180', thumbnailWidth: '220',
+        //     thumbnailAlignment: 'scaled',
+        //     thumbnailGutterWidth: 0, thumbnailGutterHeight: 0,
+        //     thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+        //     thumbnailToolbarImage: null,
+        //     thumbnailToolbarAlbum: null,
+        //     thumbnailLabel: { display: false },
+        //     galleryDisplayTransitionDuration: 1500,
+        //     thumbnailDisplayTransition: 'imageSlideUp',
+        //     thumbnailDisplayTransitionDuration: 1200,
+        //     thumbnailDisplayTransitionEasing: 'easeInOutQuint',
+        //     thumbnailDisplayInterval: 60,
+        //     thumbnailBuildInit2: 'image_scale_1.15',
+        //     thumbnailHoverEffect2: 'thumbnail_scale_1.00_1.05_300|image_scale_1.15_1.00',
+        //     touchAnimation: true,
+        //     touchAutoOpenDelay: 500,
+        //     viewerToolbar: { display: false },
+        //     viewerTools:    {
+        //       topLeft:   'label',
+        //       topRight:  'shareButton, rotateLeft, rotateRight, fullscreenButton, closeButton'
+        //     },
+        //     galleryTheme : { 
+        //       thumbnail: { background: '#111' },
+        //     },
+        //     locationHash: true
+        //   });
     
     });
 
